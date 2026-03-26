4ae41f3f74fd449aa2236beb6d6402f0f167aa7f
Dhruv Parsana <dhruvparsana40@gmail.com>
2026-03-26 12:09:39 +0530

    V2

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Counter2 from './Counter2.jsx'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Counter2/>
+    <App />
   </StrictMode>,
 )
